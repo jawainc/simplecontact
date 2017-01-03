0f35fba7c3d2a4a5fa00575f5fdab3205c8266f4
Vojta Svoboda <vojtasvoboda.cz@gmail.com>
2017-01-03 15:17:01 +0100

    Add Cs locale

---
+++ lang/cs/validation.php
@@ -0,0 +1,20 @@
+<?php return [
+    'custom' => [
+        'name' => [
+            'required' => 'Zadejte prosím vaše jméno!',
+        ],
+        'email' => [
+            'required' => 'E-mail adresa je povinná',
+            'email' => 'Zadejte prosím vaší e-mailovou adresu!'
+        ],
+        'subject' => [
+            'required' => 'Zadejte prosím předmět vaší zprávy!',
+        ],
+        'message' => [
+            'required' => 'Zadejte prosím text vaší zprávy!',
+        ],
+        'reCAPTCHA' => [
+            'required' => 'Chyba ověření, zaškrtněte prosím reCAPTCHA políčko.',
+        ],
+    ],
+];
